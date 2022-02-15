e7fcb3c5209fe2dddc1c457ed3e60a93e4c573d5
fwzlym <207194494@qq.com>
2022-02-15 22:51:30 +0800

    Initial commit

---
+++ src/main/java/com/fwzlym/boot/cloudtheater/CloudTheaterApplication.java
@@ -0,0 +1,13 @@
+package com.fwzlym.boot.cloudtheater;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudTheaterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudTheaterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fwzlym/boot/cloudtheater/CloudTheaterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fwzlym.boot.cloudtheater;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudTheaterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
